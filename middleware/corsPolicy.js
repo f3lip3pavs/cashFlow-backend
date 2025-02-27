16e9f9e21ced0ba241894ed9fda03bdb4bd63900
Felipe Pavan <felipepavanm@gmail.com>
2025-02-27 01:09:57 -0300

    updating the cors policy to accept the render service

--- middleware/corsPolicy.js
+++ middleware/corsPolicy.js
@@ -1,6 +1,15 @@
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://cash-flow-frontend.vercel.app"
+];
+
 const cors = (req, res, next) => {
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
        
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
