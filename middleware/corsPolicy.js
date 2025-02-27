113fa0b90e5125ce02f0af8887f8f98306ec454a
Felipe Pavan <felipepavanm@gmail.com>
2025-02-27 01:28:30 -0300

    updating the cors policy to accept the render service

--- middleware/corsPolicy.js
+++ middleware/corsPolicy.js
@@ -8,11 +8,11 @@ const cors = (req, res, next) => {
 
     if (allowedOrigins.includes(origin)) {
         res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        res.setHeader("Access-Control-Allow-Credentials", "true");
     }
        
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
     if(req.method == 'OPTIONS'){
         return res.sendStatus(204)
     };
